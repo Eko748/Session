0db340ef37ea4a197321a0da243c63e5370ae8b9
Eko748 <79303700+Eko748@users.noreply.github.com>
2021-05-20 09:37:15 +0700

    Tugas Session

---
+++ login.php
@@ -0,0 +1,85 @@
+<?php 
+session_start();
+include_once('db_connect.php');
+$database = new database();
+ 
+if(isset($_SESSION['is_login']))
+{
+    header('location:home.php');
+}
+ 
+if(isset($_COOKIE['username']))
+{
+  $database->relogin($_COOKIE['username']);
+  header('location:home.php');
+}
+ 
+if(isset($_POST['login']))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if(isset($_POST['remember']))
+    {
+      $remember = TRUE;
+    }
+    else
+    {
+      $remember = FALSE;
+    }
+ 
+    if($database->login($username,$password,$remember))
+    {
+      header('location:home.php');
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Login Form</title>
+ 
+
+ 
+ 
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+ 
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+  </head>
+  <body class="text-center">
+    <form class="form-signin" method="post" action="">
+  <center>
+    <fieldset>
+    <legend>  <img class="mb-4" src="assets/meubel.jpg" alt="" width="72" height="72"></legend>
+  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  <label for="username" class="sr-only">Username</label>
+  <input type="text" id="username" class="form-control" placeholder="Username" name="username" required autofocus>
+  <br>
+  <label for="password" class="sr-only">Password </label>
+  <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me" name="remember"> Remember me
+    </label>
+  </div>
+  <br>
+  <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Sign in</button>
+  <a href="register.php" class="btn btn-lg btn-success btn-block">Register</a>
+  <p class="mt-5 mb-3 text-muted">Meubel Kami &copy; 2021</p>
+  </fieldset>
+  </center>
+</form>
+</body>
+</html>
